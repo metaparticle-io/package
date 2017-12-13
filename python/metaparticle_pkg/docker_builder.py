681dab1a4aeac4b7465d974aa7059a0e44b992fc
Brendan Burns <brendan.d.burns@gmail.com>
2017-12-12 21:53:33 -0800

    deletes.

--- python/metaparticle_pkg/docker_builder.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-class DockerBuilder:
-    def build(self, img):
-        subprocess.call(['docker', 'build', '-t', img, '.'])
-
-    def publish(self, img):
-        subprocess.call(['docker', 'push', img])
