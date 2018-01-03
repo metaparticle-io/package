e961a4ce895aa3d7c1fbedc462d29e4d63b6fdf3
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-01-03 13:24:46 +0100

    parametrize name and repo in simple example
    It is easier for people to try examples.

--- python/examples/simple/example.py
+++ python/examples/simple/example.py
@@ -1,14 +1,32 @@
 from metaparticle_pkg import Containerize
+import time
+
+package_repo = 'repo'
+package_name = 'something'
+sleep_time = 10
 
 
-@Containerize(package={'name': 'something', 'repository': 'repo'}, runtime={'ports': [80, 8080]})
+@Containerize(
+    package={'name': package_name, 'repository': package_repo},
+    runtime={'ports': [80, 8080]}
+)
 def container_with_port():
     print('hello container_with_port')
 
+    for i in range(sleep_time):
+        print('Sleeping ... {} sec'.format(i))
+        time.sleep(1)
 
-@Containerize(package={'name': 'something', 'repository': 'repo', 'publish': True})
+
+@Containerize(
+    package={'name': package_name, 'repository': package_repo, 'publish': True}
+)
 def hihi():
     print('hello world!')
+
+    for i in range(sleep_time):
+        print('Sleeping ... {} sec'.format(i))
+        time.sleep(1)
 
 
 if __name__ == '__main__':
