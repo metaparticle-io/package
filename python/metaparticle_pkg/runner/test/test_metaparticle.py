0ffe7164a70887ed96698a52baa9cbe9cbd2726b
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-01-24 11:44:09 +0100

    fixing python tests for builder and runner

--- python/metaparticle_pkg/runner/test/test_metaparticle.py
+++ python/metaparticle_pkg/runner/test/test_metaparticle.py
@@ -2,7 +2,7 @@
 '''Unit tests for MetaparticleRunner'''
 
 import unittest
-from mock import patch, MagicMock, mock_open
+from unittest.mock import patch, MagicMock, mock_open
 from metaparticle_pkg.runner import metaparticle
 
 
