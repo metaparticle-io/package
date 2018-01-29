5c1640db16079aea02f738d37612a6b68fc10bc0
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-01-29 07:00:09 +0100

    Fix Python tests (#83)
    * use pytest to run tests
    
    * fixing python tests for builder and runner

--- python/metaparticle_pkg/runner/test/test_docker_runner.py
+++ python/metaparticle_pkg/runner/test/test_docker_runner.py
@@ -3,70 +3,74 @@
 
 
 import unittest
-from mock import patch, call, MagicMock
-from metaparticle_pkg.runner import docker
+from unittest.mock import patch, call, MagicMock
+from metaparticle_pkg.runner import docker_runner
 
 
 class TestDockerRunner(unittest.TestCase):
     '''Unit tests for DockerRunner'''
 
     def setUp(self):
-        self.docker_runner = docker.DockerRunner()
+        self.runner = docker_runner.DockerRunner()
 
-    @patch("metaparticle_pkg.runner.docker.subprocess")
-    def test_run(self, mocked_subprocess):
+        # mock container
+        self.cid = '123'
+        self.runner.container = MagicMock()
+        self.runner.container.get = MagicMock(return_value=self.cid)
+
+    @patch('metaparticle_pkg.runner.docker_runner.APIClient.create_container')
+    @patch('metaparticle_pkg.runner.docker_runner.APIClient.start')
+    def test_run(self, mocked_start, mocked_create):
         '''Test Run method'''
 
         # Input arguments
         img = "test_image"
         name = "test_name"
+        port = '4562'
         options = MagicMock()
-        options.ports = ["4562"]
+        options.ports = [port]
 
         # Expected argument called with
-        expected_args = [
-            'docker', 'run', '-d',
-            '--name', 'test_name',
-            '-p', '4562:4562', 'test_image'
-        ]
+        self.runner.run(img, name, options)
 
-        self.docker_runner.run(img, name, options)
+        mocked_create.assert_called_once_with(
+            'test_image',
+            host_config={
+                'NetworkMode': 'default',
+                'PortBindings': {
+                    '{}/tcp'.format(port): [
+                        {
+                            'HostIp': '',
+                            'HostPort': port,
+                        }
+                    ]
+                }
+            },
+            name='test_name',
+            ports=[port]
+        )
 
-        mocked_subprocess.check_call.assert_called_once_with(
-            expected_args)
+        mocked_start.assert_called_once()
 
-    @patch("metaparticle_pkg.runner.docker.subprocess")
-    def test_logs(self, mocked_subprocess):
+    @patch('metaparticle_pkg.runner.docker_runner.APIClient.logs')
+    def test_logs(self, mocked_logs):
         '''Test logs method'''
 
-        # Input arguments
-        name = "test_name"
+        self.runner.logs()
+        mocked_logs.assert_called_once_with('123', follow=True, stream=True)
 
-        # Expected argument called with
-        expected_args = ['docker', 'logs', '-f', name]
-
-        self.docker_runner.logs(name)
-
-        mocked_subprocess.check_call.assert_called_once_with(
-            expected_args)
-
-    @patch("metaparticle_pkg.runner.docker.subprocess")
-    def test_cancel(self, mocked_subprocess):
+    @patch('metaparticle_pkg.runner.docker_runner.APIClient.kill')
+    @patch('metaparticle_pkg.runner.docker_runner.APIClient.remove_container')
+    def test_cancel(self, mocked_remove, mocked_kill):
         '''Test cancel method'''
 
         # Input arguments
         name = "test_name"
 
-        # Expected calls with supplied arguments
-        expected_call_list = [
-            call(['docker', 'kill', name]),
-            call(['docker', 'rm', name])
-        ]
+        self.runner.cancel(name)
 
-        self.docker_runner.cancel(name)
-
-        mocked_subprocess.check_call.assert_has_calls(
-            expected_call_list)
+        mocked_kill.assert_called_with(self.cid)
+        mocked_remove.assert_called_with(self.cid)
 
 
 if __name__ == '__main__':
