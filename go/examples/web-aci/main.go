78ae23684d014e010d34d55f08a96e92759d6946
Radu Matei <matei.radu94@gmail.com>
2018-01-29 17:11:53 +0200

    Add ACI example

---
+++ go/examples/web-aci/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/metaparticle-io/package/go/metaparticle"
+)
+
+var port int32 = 80
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	hostname, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello metaparticle in Azure Container Instances from %s %s!\n", r.RequestURI, hostname)
+	fmt.Printf("Request received: %s\n", r.RequestURI)
+}
+
+func main() {
+	metaparticle.Containerize(
+		&metaparticle.Runtime{
+			Executor:      "aci",
+			Ports:         []int32{port},
+			PublicAddress: true,
+		},
+		&metaparticle.Package{
+			Name:       "metaparticle-aci-demo",
+			Repository: "radumatei",
+			Builder:    "docker",
+			Verbose:    true,
+			Publish:    true,
+		},
+		func() {
+			log.Printf("Starting server on :%d\n", port)
+			http.HandleFunc("/", handler)
+			err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+			if err != nil {
+				log.Fatal("Couldn't start the server: ", err)
+			}
+		})
+}
