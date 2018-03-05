0ae219a8870643ae29407dd7b81cf15cc5b7ca7d
Leigh Ciechanowski <leigh.ciechanowski@sky.uk>
2018-03-05 17:57:09 +0000

    Create unit test project and added tests for Metaparticle.Package.Config

---
+++ dotnet/Metaparticle.Package.Tests/MetaparticleTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using Metaparticle.Package;
+
+namespace Metaparticle.Package.Tests
+{
+    public class MetaparticleTests
+    {
+        [Fact]
+        public void Config_Loads_Environment_Variables()
+        {
+            Environment.SetEnvironmentVariable("METAPARTICLE_CONFIG_REPOSITORY", "testRepo");
+            Environment.SetEnvironmentVariable("METAPARTICLE_CONFIG_PUBLISH", "true");
+            var config = new Config();
+            Assert.True("testRepo" == config.Repository);
+            Assert.True(true == config.Publish); 
+        }
+
+        [Fact]
+        public void Config_Defaults_Builder_To_Docker()
+        {
+            var config = new Config();
+            Assert.True("docker" == config.Builder);
+        }
+    }
+}
