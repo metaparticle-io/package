6002522eb1a8339a629fc659dddcb449eda3741a
George Harley <gharley@pivotal.io>
2018-03-25 05:29:16 +0100

    Support containerizing of Spring Boot apps (#115)
    * Necessary to upgrade PowerMock version in order to run tests
    * Additions to gitignore file

--- java/src/main/java/io/metaparticle/Metaparticle.java
+++ java/src/main/java/io/metaparticle/Metaparticle.java
@@ -1,19 +1,26 @@
 package io.metaparticle;
 
-import static io.metaparticle.Util.handleErrorExec;
-import static io.metaparticle.Util.once;
-
+import io.metaparticle.annotations.Package;
+import io.metaparticle.annotations.Runtime;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.logging.Logger;
 
-import io.metaparticle.annotations.Package;
-import io.metaparticle.annotations.Runtime;
+import static io.metaparticle.Util.handleErrorExec;
+import static io.metaparticle.Util.once;
 
 public class Metaparticle {
+
+    private static Logger logger = Logger.getLogger(Metaparticle.class.getName());
+
+    static final String SPRING_BOOT_APP_ANNOTATION = "org.springframework.boot.autoconfigure.SpringBootApplication";
+
+    static final String SPRING_BOOT_LOADER_JAR_LAUNCHER_CLASS = "org.springframework.boot.loader.JarLauncher";
 
     public static boolean inDockerContainer() {
         String envFlag = System.getenv("METAPARTICLE_IN_CONTAINER");
@@ -32,7 +39,7 @@ public class Metaparticle {
         if (f.exists()) {
             try {
                 String s = new String(Files.readAllBytes(f.toPath()), "UTF-8");
-                return (s.indexOf("docker") != -1 || s.indexOf("kubepods") != -1);
+                return (s.contains("docker") || s.contains("kubepods"));
             } catch (IOException ex) {
                 throw new IllegalStateException(ex);
             }
@@ -68,13 +75,13 @@ public class Metaparticle {
         }
     }
 
-    public static void writeDockerfile(String className, Package p) throws IOException {
+    public static void writeDockerfile(String className, Package p, boolean runSpringBootJar) throws IOException {
         byte [] output;
-        if (p.dockerfile() == null || p.dockerfile().length() == 0) {
-            String contents = 
+        if (p.dockerfile().isEmpty()) {
+            String contents =
 "FROM openjdk:8-jre-alpine\n" +
 "COPY %s /main.jar\n" +
-"CMD java -classpath /main.jar %s";
+(runSpringBootJar ? "CMD java -jar /main.jar" : "CMD java -classpath /main.jar %s");
             output = String.format(contents, p.jarFile(), className).getBytes();
         } else {
             output = Files.readAllBytes(Paths.get(p.dockerfile()));
@@ -98,7 +105,6 @@ public class Metaparticle {
             StackTraceElement[] traces = Thread.currentThread().getStackTrace();
             String className = traces[2].getClassName();
             String methodName = traces[2].getMethodName();
-            
 
             try {
                 Class<?> clazz = Class.forName(className);
@@ -118,10 +124,10 @@ public class Metaparticle {
                 OutputStream stdout = packageAnnotation.verbose() ? System.out : null;
                 OutputStream stderr = packageAnnotation.quiet() ? null : System.err;
 
-                writeDockerfile(className, packageAnnotation);
+                writeDockerfile(className, packageAnnotation, isSpringBootAppBeingContainerized(clazz));
 
                 if (packageAnnotation.build()) {
-                    handleErrorExec(new String[] {"mvn", "package"}, System.out, System.err);                    
+                    doPackage(isRunningAsSpringBootApplication(traces), targetJarExists(packageAnnotation.jarFile()));
                     builder.build(".", image, stdout, stderr);
                     if (packageAnnotation.publish()) {
                         builder.push(image, stdout, stderr);
@@ -138,6 +144,30 @@ public class Metaparticle {
                 // This should really never happen.
                 throw new IllegalStateException(ex);
             }
+        }
+    }
+
+    static boolean isSpringBootAppBeingContainerized(Class<?> containerizedClass) {
+        return Arrays.stream(containerizedClass.getAnnotations())
+                .anyMatch(annotation -> annotation.toString().contains(SPRING_BOOT_APP_ANNOTATION));
+    }
+
+    static boolean targetJarExists(String jarfileName) {
+        return Files.exists(Paths.get(jarfileName));
+    }
+
+    static boolean isRunningAsSpringBootApplication(StackTraceElement[] traces) {
+        String launchClass = traces[traces.length - 1].getClassName();
+        String launchMethod = traces[traces.length - 1].getMethodName();
+        return launchClass.equals(SPRING_BOOT_LOADER_JAR_LAUNCHER_CLASS) && launchMethod.equals("main");
+    }
+
+    static boolean doPackage(boolean inRunningSpringBootApp, boolean targetExists) {
+        if (inRunningSpringBootApp && targetExists) {
+            logger.info("Package step skipped when running as Spring Boot Jar and target already exists");
+            return false;
+        } else {
+            return handleErrorExec(new String[]{"mvn", "package"}, System.out, System.err);
         }
     }
 }
