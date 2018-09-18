3154e75a9bc7fb985ab25b1dcbd6918544588c99
Ben Woodhouse <bwoodhouse322@gmail.com>
2018-09-18 05:52:40 +0100

    more efficient base image (#131)
    * more efficient base image
    
    * squashed layers
    
    * removed breaking character
    
    * adding certs
    
    * sized down to 6mb and tested with simple http server

--- go/metaparticle/metaparticle.go
+++ go/metaparticle/metaparticle.go
@@ -103,19 +103,13 @@ func builderFromPackage(pkg *Package) (Builder, error) {
 
 func writeDockerfile(name string) error {
 	contents := `FROM golang:1.9 as builder
-WORKDIR /go/src/app
-COPY . .
-
-RUN go get -u github.com/golang/dep/cmd/dep
-RUN dep init
-RUN go-wrapper install
-
-
-FROM ubuntu
-
-COPY --from=builder /go/bin/app .
-
-CMD ["./app"]
+	WORKDIR /go/src/app
+	COPY . .
+	RUN go get -u github.com/golang/dep/cmd/dep && dep init && CGO_ENABLED=0 GOOS=linux go-wrapper install 
+	FROM alpine:3.7
+	RUN apk add --no-cache --update ca-certificates 
+	COPY --from=builder /go/bin/app .
+	CMD ["./app"]
 `
 	return ioutil.WriteFile("Dockerfile", []byte(contents), 0644)
 }
